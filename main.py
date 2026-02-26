524b4514a296c75e64c5c3899519bbaeedf9b10f
Xudoyberdi <xudoyberdirajabov222@gmail.com>
2026-02-26 13:11:38 +0500

    main.py yangilandi

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-a = int(input("Matn kirit"))
+from random import randint
+class Human:
+    def __init__(self, name):
+        self.name = name
+        self.ID = randint(1000, 9999)
+    
+    def get(self):
+        return self.name, self.ID
+
+ism = "xudoyberdi"
+
+ism1 = Human(ism)
+print(ism1.get())
